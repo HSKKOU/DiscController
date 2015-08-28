1b2541b71a28ac68285875dfcaec159b5e446fac
HSKKOU <hskk1120551@gmail.com>
2015-08-28 22:36:44 +0900

    Create Singleton and add that

--- Assets/Scripts/Managers/DiscManager.cs
+++ Assets/Scripts/Managers/DiscManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DiscController : MonoBehaviour {
+public class DiscManager : SingletonMono<DiscManager> {
 
 	// Use this for initialization
 	void Start () {
